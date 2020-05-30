bbe340f183ea782e8c691e5a6500fa9bb2bddb38
juju4 <juju4@users.noreply.github.com>
2020-05-30 10:13:37 -0400

    test/default: greenlight check

--- test/integration/default/serverspec/bbb_spec.rb
+++ test/integration/default/serverspec/bbb_spec.rb
@@ -119,6 +119,16 @@ describe command('curl -vk https://localhost/pad/') do
 end
 
 # if greenlight
+describe command('docker run --rm --env-file /root/greenlight/.env bigbluebutton/greenlight:v2 bundle exec rake conf:check') do
+  its(:stdout) { should match /Checking environment: Passed/ }
+  its(:stderr) { should_not match /No such file or directory/ }
+  # No greenlight option to validate self-signed certificate so following test will fail in test mode
+  # will also result in user error after login 'Error connecting to BigBlueButton server'
+  its(:stdout) { should_not match /Checking Connection: Failed/ }
+  its(:stdout) { should_not match /Error connecting to BigBlueButton server/ }
+  its(:exit_status) { should eq 0 }
+end
+
 describe command('curl -vk https://localhost/b/') do
   its(:stdout) { should match /BigBlueButton/ }
   its(:stdout) { should match /Welcome to BigBlueButton./ }
